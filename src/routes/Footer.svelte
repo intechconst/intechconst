8feca562b2c5536c74c9c1def3d35a869ef4773d
Киряев Кирилл <kiryaev.developer@gmail.com>
2022-09-22 19:38:43 +0500

    Create Footer.svelte

---
+++ src/routes/Footer.svelte
@@ -0,0 +1,21 @@
+<footer>
+    <span class="copyright">
+        © 2022 ООО "ПромТехСтрой"
+    </span>
+</footer>
+<style lang="scss">
+    @import "../sizes.scss";
+    @import "../colors.scss";
+
+    footer {
+        height: $footer-height;
+        background: $footer-color;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .copyright {
+        color: $text-light;
+    }
+</style>
