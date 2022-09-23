d7a59f2a82e888e8c8e60c2aa3185ff4e3822ae5
Киряев Кирилл <kiryaev.developer@gmail.com>
2022-09-24 01:29:44 +0500

    Add about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Container from "$lib/Container.svelte";
+
+</script>
+<Container>
+    <div class="content">
+        <h2>О нас</h2>
+    </div>
+</Container>
+<style lang="scss">
+    @import "../../sizes.scss";
+
+    .content {
+        padding-top: $header-height + $spacing;
+    }
+</style>
